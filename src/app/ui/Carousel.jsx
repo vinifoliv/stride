b3d69145144e4f055caf5e1661e25df6b2207381
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-09 22:46:20 -0300

    Menu, Card, and Carousel components added. Database structure scratch created.

---
+++ src/app/ui/Carousel.jsx
@@ -0,0 +1,10 @@
+import ArticleCard from "./Card";
+import styles from './Carousel.module.css';
+
+export default function Carousel(articles) {
+    return (
+        <div className={styles.card}>
+
+        </div>
+    );
+}
---
+++ src/app/ui/Menu.js
@@ -0,0 +1,9 @@
+import styles from './Menu.module.css';
+
+export default function Menu() {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>Stride</h1>
+        </header>
+    );
+}
